4b15239ec04f265b5c80bf30625e71d228a22c12
Shivam Gupta <guptashivam8005@gmail.com>
2023-09-01 19:08:11 +0530

    first commit

---
+++ app/db.js
@@ -0,0 +1,12 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const mongoose = require('mongoose');
+
+exports.connectMongoose =()=>{
+    mongoose.set("strictQuery", false);
+    mongoose.connect('mongodb+srv://guptashivam:sam123@cluster0.2mysvv6.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true })
+    .then((e)=>console.log("Connected to Mongodb => Habit-Tracker"))
+    .catch((e)=>console.log("Not Connect Mongodb", e));
+}
